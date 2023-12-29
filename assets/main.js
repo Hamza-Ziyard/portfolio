a2629c81a6759e085a5fbfac619a24e7f2a42ade
Hamza-Ziyard <hamzaziyard.ux@gmail.com>
2023-12-29 14:12:59 +0530

    added desktop code

---
+++ assets/main.js
@@ -0,0 +1,12 @@
+<script>
+$(document).ready(function() {
+    $(".project-thumbnail").hover(
+        function() {
+            $(this).css("cursor", "url(img/Frame\ 81.png), auto");
+        },
+        function() {
+            $(this).css("cursor", "auto");
+        }
+    );
+});
+</script>
